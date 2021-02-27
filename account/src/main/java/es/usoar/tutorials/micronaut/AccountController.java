c9592e81f413a999b7e56e88030147f8c4c41fcf
amhg <amhg@mail.com>
2021-02-27 20:03:30 +0100

    Added Swagger Config

--- account/src/main/java/es/usoar/tutorials/micronaut/AccountController.java
+++ account/src/main/java/es/usoar/tutorials/micronaut/AccountController.java
@@ -4,15 +4,27 @@ import io.micronaut.context.event.ApplicationEventPublisher;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 import javax.inject.Inject;
 import java.util.List;
 import java.util.UUID;
 
-
-@Controller("/users")
+@OpenAPIDefinition(
+  info = @Info(
+    title = "account-server",
+    version = "0.1",
+    description = "My API",
+    license = @License(name = "Apache 2.0", url = "https://foo.bar"),
+    contact = @Contact(url = "https://gigantic-server.com", name = "Fred", email = "Fd@mail.com")
+  )
+)
+@Controller("/account")
 public class AccountController {
 
   @Inject
---
+++ startup/src/main/java/es/usoar/tutorials/micronaut/swagger/SwaggerController.java
@@ -0,0 +1,27 @@
+package es.usoar.tutorials.micronaut.swagger;
+
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.validation.Validated;
+import io.micronaut.views.View;
+import io.swagger.v3.oas.annotations.Hidden;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+
+@Hidden
+@Controller("/api")
+@Validated
+public class SwaggerController {
+
+  @Inject
+  SwaggerConfig config;
+
+  @View("views.swagger/index")
+  @Get
+  public SwaggerConfig index() {
+    LoggerFactory.getLogger(SwaggerController.class).info("Trying to render swagger-view");
+    return config;
+  }
+}
